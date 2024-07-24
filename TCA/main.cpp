a38bbb5913049f2b61f45495979ec1133c5d671f
gmrcWP <jorgesick.roch@gmail.com>
2024-07-23 22:53:19 -0400

    deltaTime

--- TCA/main.cpp
+++ TCA/main.cpp
@@ -2,22 +2,36 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //-----------------INITIALIZE----------------------
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!", sf::Style::Default, settings);
+    window.setFramerateLimit(360);
+    //-----------------INITIALIZE----------------------
 
+    //-----------------INITIALIZE----------------------
+    //-----------------INITIALIZE----------------------
+
+    //-----------------LOAD----------------------
+    //-----------------LOAD----------------------
+    sf::Clock clock;
     while (window.isOpen())
     {
+        sf::Time deltaTimeTimer = clock.restart();
+        float deltaTime = deltaTimeTimer.asMicroseconds() / 1000.0;
+        //-----------------UPDATE----------------------
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        //-----------------UPDATE----------------------
 
-        window.clear();
-        window.draw(shape);
+        //-----------------DRAW----------------------
+        window.clear(sf::Color::Black);
         window.display();
+        //-----------------DRAW----------------------
     }
 
     return 0;
